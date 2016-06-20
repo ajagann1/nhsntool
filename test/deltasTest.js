fc7722d930070958dcc76c002a3fe6e527471c48
Arvind <Arvind>
2016-06-20 15:30:09 -0400

    Creating unit tests and some sample data.

---
+++ test/deltasTest.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Arvind on 6/20/2016.
+ */
+
+var assert = require('assert');
+var deltas = require('../deltas');
+var fs = require('fs');
+
+describe('deltas', function(){
+  describe('#parseFile', function(){
+    it('should return an array of the parsed values within the given file', function(done){
+      //Check to make sure that all of the information from the rows in the file is being read.
+      try {
+        var parsed = deltas.parseFile(fs.readFileSync('test/data/conceptTest.txt', 'utf-8'), '\t');
+        assert(parsed, 'Expected a parsed file');
+        assert(parsed.length === 13, 'Incorrect amount of information parsed');
+        assert(parsed[0].delimited.length === 5, 'Incorrect amount of columns parsed');
+        done();
+      }catch(err){
+        done(err);
+      }
+    });
+  });
+  describe('#searchDescript', function(){
+    it('should return any detected changes to the coded concept entry currently being examined', function(done){
+      try{
+        var parsedConcepts = deltas.parseFile(fs.readFileSync('test/data/conceptTest.txt', 'utf-8'), '\t');
+        var parsedDescripts = deltas.parseFile(fs.readFileSync('test/data/descriptTest.txt', 'utf-8'), '\t');
+        var conceptCode = parsedConcepts[1].delimited[0];
+        var str = deltas.searchDescript(conceptCode, parsedDescripts, false);
+        assert(str, "Couldn't find the specified concept code");
+        assert(str.match(/\n/g).length === 2, "Didn't find the appropriate amount of descriptions in the test file")
+        done();
+      }catch(err){
+        done(err);
+      }
+    });
+  });
+});
