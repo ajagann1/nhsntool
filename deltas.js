7a2bb6180232c7eb3b65e9915fb58b3b9f066d18
Arvind <Arvind>
2016-06-22 11:58:17 -0400

    Large changes to handle the excel file and output results as an html with a list of collapsible concept code entries

--- deltas.js
+++ deltas.js
@@ -34,48 +34,91 @@ exports.parseFile = function (file, delimiter) {
   return array;
 };
 
+var searchConcept = function (conceptCode, concepts, ci){
+  var html = '';
+  for (var i in concepts) {
+    //If the user indicates there's no header, parse the first line, else, don't.
+    if (i === '0') {
+      var conceptHeader = concepts[i].delimited;
+      continue;
+    }
+    if(conceptCode === concepts[i].delimited[ci]) {
+      html += '<div data-role="collapsible"><h4>Concepts Delta</h4>';
+      html += '<table><tbody><tr>';
+      for(var j in conceptHeader) {
+         html += '<th>' + conceptHeader[j] + '</th>';
+      }
+      html += '</tr><tr>';
+      for(var j in concepts[i].delimited){
+        html += '<td>' + concepts[i].delimited[j] + '</td>';
+      }
+      html += '</tr></tbody></table></div>';
+      break;
+    }
+  }
+  return html;
+};
+
 //Search the description delta file for the currently looked at concept code.
-exports.searchDescript = function (conceptCode, descript, header, di) {
-  var str = '';
+var searchDescript = function (conceptCode, descript, di) {
+  var html = '';
   var found = false;
   var descriptHeader = '';
   for (var i in descript) {
-    if (header && i === '0') {
-      descriptHeader = descript[i].original + '\n';
+    if (i === '0') {
+      descriptHeader = descript[i].delimited;
       continue;
     }
     if (conceptCode === descript[i].delimited[di]) {
       if (!found) {
-        str += 'Description:\n';
-        str += descriptHeader;
+        html += '<div data-role="collapsible"><h4>Descriptions Delta</h4>';
+        html += '<table><tbody><tr>';
+        for(var j in descriptHeader) {
+          html += '<th>' + descriptHeader[j] + '</th>';
+        }
+        html += '</tr>';
         found = true;
       }
-      str += descript[i].original + '\n';
+      html += '<tr>';
+      for(var j in descript[i].delimited) {
+        html += '<td>' + descript[i].delimited[j] + '</td>';
+      }
+      html += '</tr>';
     }
   }
-  return str;
+  if(found) html += '</tbody></table></div>';
+  return html;
 };
 
 //Search the relationshp delta file for the currently looked at concept code.
-exports.searchRelation = function (conceptCode, relation, header, ri) {
-  var str = '';
+var searchRelation = function (conceptCode, relation, ri) {
+  var html = '';
   var found = false;
   var relationHeader = '';
   for (var i in relation) {
-    if (header && i === '0') {
-      relationHeader = relation[i].original + '\n';
+    if (i === '0') {
+      relationHeader = relation[i].delimited;
       continue;
     }
     if (conceptCode === relation[i].delimited[ri.first] || conceptCode === relation[i].delimited[ri.second]) {
       if (!found) {
-        str += 'Relationship:\n';
-        str += relationHeader;
+        html += '<div data-role="collapsible"><h4>Relationships Delta</h4>';
+        html += '<table><tbody><tr>';
+        for(var j in relationHeader) {
+          html += '<th>' + relationHeader[j] + '</th>';
+        }
+        html += '</tr>';
         found = true;
       }
-      str += relation[i].original + '\n';
+      html += '<tr>';
+      for(var j in relation[i].delimited) {
+        html += '<td>' + relation[i].delimited[j] + '</td>';
+      }
+      html += '</tr>';
     }
   }
-  return str;
+  if(found) html += '</tbody></table></div>';
+  return html;
 };
 
 //Determine if the input for the delimiter flag is usable. Throw an error if it isn't.
@@ -88,37 +131,32 @@ exports.parseDelimiter = function (delimit) {
   else throw 'Unsupported delimiter choice ' + delimit + '.';
 };
 
-//Determine if the input for the header flag is usable. Throw an error if it isn't.
-exports.parseHeader = function (header) {
-  if (header.toLowerCase() === 'y') {
-    return true;
-  } else if (header.toLowerCase() === 'n') {
-    return false;
-  } else {
-    throw 'Header input must be y or n';
-  }
-};
+exports.generateOutput = function (conceptCode, concepts, descript, relation, indices) {
+  var html = '';
 
-exports.generateOutput = function (conceptCode, descript, relation, header, indices) {
+  var searchC = searchConcept(conceptCode, concepts, indices.ci);
+  if(searchC) html += searchC;
 
-  var str = '';
+  var searchD = searchDescript(conceptCode, descript, indices.di);
+  if (searchD) html += searchD;
 
-  var searchD = exports.searchDescript(conceptCode, descript, header, indices.di);
-  if (searchD) str += '\n\n' + searchD;
-  else str += '\n';
-
-  var searchR = exports.searchRelation(conceptCode, relation, header, indices.ri);
-  if (searchR) str += '\n' + searchR;
-
-  return str;
+  var searchR = searchRelation(conceptCode, relation, indices.ri);
+  if (searchR) html += searchR;
+  
+  return html;
 };
 
 exports.parseOutputLoc = function (outputLoc, force) {
+  var filepath;
   try {
-    var filepath;
+    if(outputLoc.indexOf('.') === -1) outputLoc += '.html';
+    if(outputLoc.substring(outputLoc.lastIndexOf('.'), outputLoc.length) !== '.html')
+      outputLoc.replace(outputLoc.substring(outputLoc.lastIndexOf('.'), outputLoc.length), '.html');
+
     if (outputLoc.indexOf('/') !== -1) filepath = outputLoc.substring(0, outputLoc.lastIndexOf('/'));
     else if (outputLoc.indexOf('\\') !== -1) filepath = outputLoc.substring(0, outputLoc.lastIndexOf('\\'));
     else throw "Incorrect path";
+
     var stats = fs.statSync(filepath);
   } catch (err) {
     if (force) {
@@ -133,26 +171,49 @@ exports.parseOutputLoc = function (outputLoc, force) {
   return outputLoc;
 };
 
-exports.parseIndices = function (concepts, descript, relation, header, indices){
+exports.parseIndices = function (concepts, descript, relation, ci, di, ri, xi){
   //Generic test cases
-  if(!header && !indices) throw "If there's no header, indices need to be specified";
-
-  if(indices.indexOf(',') === -1) throw "Incorrect input for indices. Not splitting indices by comma";
-  if(indices.match(/,/g).length !== 3) throw "Incorrect amount of indices inputted.";
-  
-  var array = indices.split(',');
-  
-  if(array[0] - 1 > concepts[0].delimited.length - 1 || array[0] - 1 < 0) throw "Concept index is not a valid index";
-  else if(array[1] - 1 > descript[0].delimited.length - 1 || array[1] - 1 < 0) throw "Description index is not a valid index";
-  else if(array[2] - 1 > relation[0].delimited.length - 1 || array[2] - 1< 0) throw "First relationship index is not a valid index";
-  else if(array[3] - 1 > relation[0].delimited.length - 1 || array[3] - 1< 0) throw "Second relationship index is not a valid index";
+  if(ci && (ci - 1 > concepts[0].delimited.length - 1 || ci - 1 < 0)) throw "Concept index is not a valid index";
+  if(di && (di - 1 > descript[0].delimited.length - 1 || di - 1 < 0)) throw "Description index is not a valid index";
+  if(ri && (ri.indexOf(',') === -1 || ri.match(/,/g).length > 1)) throw "Incorrect input for relationship index. Not " +
+  "splitting indices by comma or too many indices inputted.";
+  if(ri){
+    var riInd = ri.split(',');
+    if(riInd[0] - 1 > relation[0].delimited.length - 1 || riInd[0] - 1 < 0) throw "First relationship index is not a valid index";
+    if(riInd[1] - 1 > relation[0].delimited.length - 1 || riInd[1] - 1 < 0) throw "Second relationship index is not a valid index";
+  }
+  if(xi && (xi.match(/\d+/g).length > 0)) throw "Inputted excel index of column isn't a possible index.";
   
   return {
-    ci: array[0] - 1,
-    di: array[1] - 1,
-    ri: {
-      first: array[2] - 1,
-      second: array[3] - 1
-    }
+    ci: ci ? ci - 1 : 0,
+    di: di ? di - 1 : 4,
+    ri: ri ? {
+      first: riInd[0] - 1,
+      second: riInd[1] - 1
+    } : {
+      first: 4,
+      second: 5
+    },
+    xi: xi ? xi.toUpperCase() : "AA"
   };
 };
+
+exports.excelArray = function(workbook, col) {
+
+  var excelConvert = function(sheet, col){
+    var array = [];
+    for(var i in sheet){
+      if(i.indexOf(col) !== -1 && !i.replace(col, '').match(/[A-Z]/g)){
+        array.push(sheet[i].v.toString());
+      }
+    }
+    if(!array) throw "Inputted excel index is invalid.";
+    return array;
+  };
+
+  var result = [];
+  result = excelConvert(workbook.Sheets[workbook.SheetNames[0]], col);
+
+  return result;
+};
+
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 /**
  * Created by Arvind on 6/20/2016.
  */
@@ -9,71 +11,85 @@ var argv = require('yargs')
   .demand('d')
   .alias('r', 'rFilePath')
   .demand('r')
-  .alias('h', 'header')
   .alias('l', 'delimiter')
   .alias('o', 'outputLoc')
   .argv;
 var fs = require('fs');
 var deltas = require('./deltas');
+var xlsx = require('xlsx');
 
 var main = function() {
   //Handle all input arguments and parse the input files. Defaulting logic used for the optional flags header and
   //delimiter.
   try {
-    var header = argv.header ? deltas.parseHeader(argv.header) : true;
-
     var delimiter = argv.delimiter ? deltas.parseDelimiter(argv.delimiter) : '\t';
 
     //Made this happen synchronously so that we populate these variables immediately
     console.log("Parsing files.");
+
+    var indices = deltas.parseIndices(concepts, descript, relation, argv.ci, argv.di, argv.ri, argv.xi);
+    
+    var file = xlsx.readFile(argv.x);
+    var excel = deltas.excelArray(file, indices.xi);
     var concepts = deltas.parseFile(fs.readFileSync(argv.cFilePath, 'utf-8'), delimiter);
     var descript = deltas.parseFile(fs.readFileSync(argv.dFilePath, 'utf-8'), delimiter);
     var relation = deltas.parseFile(fs.readFileSync(argv.rFilePath, 'utf-8'), delimiter);
-    if(!header || argv.i){
-      var indices = deltas.parseIndices(concepts, descript, relation, header, argv.i);
-    }else{
-      indices = {
-        ci: 0,
-        di: 4,
-        ri: {
-          first: 4,
-          second: 5
-        }
-      };
-    }
+
   } catch (err) {
+
     console.log(err);
     return;
+
   }
 
   var conceptHeader = '';
-  var str = '';
-  var spacer = '--------------------------------------';
-  str += spacer;
-  for (var i in concepts) {
-    //If the user indicates there's no header, parse the first line, else, don't.
-    if (header && i === '0') {
-      conceptHeader = concepts[i].original;
+  var excelHeader = '';
+  var conceptCodeArray = [];
+  var html =
+    '<html>' +
+      '<head>' +
+        '<style>' +
+          'table, th, td {' +
+            'border: 1px solid black;' +
+            'border-collapse: collapse;' +
+          '}' +
+          'table {' +
+            'width: 100%;' +
+          '}' +
+        '</style>' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1">' +
+        '<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">' +
+        '<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>' +
+        '<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>' +
+      '</head>' +
+      '<body>' +
+        '<div data-role="page" id="pageone">' +
+          '<div data-role="header">' +
+            '<h1>Results</h1>' +
+          '</div>' +
+          '<div data-role="main" class="ui-content">';
+  console.log("Generating output.");
+  for(var i in excel){
+    if(i === "0") {
+      excelHeader = excel[i];
       continue;
     }
-
-    console.log("Generating output.");
-    str += '\n\nConcept:\n';
-    str += conceptHeader + '\n';
-    str += concepts[i].original;
-
-    //Generates the output text using some conditionals for new line formatting.
-    var conceptCode = concepts[i].delimited[indices.ci];
-    str += deltas.generateOutput(conceptCode, descript, relation, header, indices);
-
-    str += '\n' + spacer;
+    if(conceptCodeArray.indexOf(excel[i]) === -1){
+      conceptCodeArray.push(excel[i]);
+      var output = deltas.generateOutput(excel[i], concepts, descript, relation, indices);
+      if(output){
+        html += '<div data-role="collapsible"><h4>' + excel[i] + '</h4>' + output + '</div>';
+      }
+    }
   }
+  html += '</div></body></html>';
 
   //Outputs the resulting delta text to a specified file
   try {
     var force = argv.fo ? true : false;
-    var outputLoc = argv.outputLoc ? deltas.parseOutputLoc(argv.outputLoc, force) : 'results.txt';
-    fs.writeFileSync(outputLoc, str);
+    var outputLoc = argv.outputLoc ? deltas.parseOutputLoc(argv.outputLoc, force) : 'results.html';
+
+    fs.writeFileSync(outputLoc, html);
     console.log('File successfully generated.');
   } catch (err) {
     console.log(err);
